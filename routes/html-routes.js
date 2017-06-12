090f224dc04092c782e9f270011189e2abd36790
btarman <btarman75@gmail.com>
2017-06-12 19:52:48 -0400

    hbs-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,15 @@ const path = require('path');
 // these are some initial html routes that we had talked about on Monday
 module.exports = function (app) {
   app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/main-page.html'));
+    res.sendFile(path.join(_dirname, '../views/index.hbs'));
   });
   app.get('/signup', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/signup.html'));
+    res.sendFile(path.join(_dirname, '../views/signup.hbs'));
   });
   app.get('/user', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/user.html'));
+    res.sendFile(path.join(_dirname, '../views/user.hbs'));
   });
   app.get('/user-item', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/user-item.html'));
+    res.sendFile(path.join(_dirname, '../views/user-item.hbs'));
   });
 };
