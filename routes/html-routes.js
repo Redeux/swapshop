d0de092eaa0fbbf242a07dd3522354907da0e2ee
E. Seton Raynor <esetonraynor@gmail.com>
2017-06-13 10:22:20 -0400

    front end connection

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,12 @@ const path = require('path');
 // these are some initial html routes that we had talked about on Monday
 module.exports = function (app) {
   app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/index.hbs'));
+    res.render(path.join(__dirname, '../views/index.hbs'));
   });
   app.get('/signup', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/signup.hbs'));
+    res.render(path.join(__dirname, '../views/signup.hbs'));
   });
   app.get('/user', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/user.hbs'));
-  });
-  app.get('/user-item', (req, res) => {
-    res.sendFile(path.join(_dirname, '../views/user-item.hbs'));
+    res.render(path.join(__dirname, '../views/user.hbs'));
   });
 };
