ca53db9bf1d1d801ae080f3e18f7aa770840158e
Phil Sautter <psautter@gmail.com>
2017-06-14 20:56:49 -0400

    bare bones working user auth

--- models/users.js
+++ models/users.js
@@ -1,55 +1,55 @@
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define('User', {
-    username: {
-      type: DataTypes.STRING(15),
-      allowNull: false,
-      unique: true,
-    },
+    // username: {
+    //   type: DataTypes.STRING(15),
+    //   allowNull: false,
+    //   unique: true,
+    // },
     password: {
       type: DataTypes.STRING(64),
       allowNull: false,
     },
-    emailAddress: {
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
       isEmail: true,
       unique: true,
     },
-    firstName: {
-      type: DataTypes.STRING(25),
-      allowNull: false,
-      is: ['^[a-z]+$', 'i'],
-    },
-    lastName: {
-      type: DataTypes.STRING(25),
-      allowNull: false,
-      is: ['^[a-z]+$', 'i'],
-    },
-    streetAddress: {
-      type: DataTypes.STRING(60),
-      allowNull: false,
-    },
-    city: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    state: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-    },
-    zipcode: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: 'beginTime',
-      defaultValue: sequelize.literal('NOW()'),
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.literal('NOW()'),
-    },
+    // firstName: {
+    //   type: DataTypes.STRING(25),
+    //   allowNull: true,
+    //   is: ['^[a-z]+$', 'i'],
+    // },
+    // lastName: {
+    //   type: DataTypes.STRING(25),
+    //   allowNull: true,
+    //   is: ['^[a-z]+$', 'i'],
+    // },
+    // streetAddress: {
+    //   type: DataTypes.STRING(60),
+    //   allowNull: true,
+    // },
+    // city: {
+    //   type: DataTypes.STRING(30),
+    //   allowNull: true,
+    // },
+    // state: {
+    //   type: DataTypes.STRING(20),
+    //   allowNull: true,
+    // },
+    // zipcode: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: true,
+    // },
+    // createdAt: {
+    //   type: DataTypes.DATE,
+    //   field: 'beginTime',
+    //   defaultValue: sequelize.literal('NOW()'),
+    // },
+    // updatedAt: {
+    //   type: DataTypes.DATE,
+    //   defaultValue: sequelize.literal('NOW()'),
+    // },
   }, {
     timestamps: true,
   }, {
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const authController = require('../contollers/authController');
-const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = (app, passport) => {
+
   app.get('/signup', authController.signup);
 
   app.get('/signin', authController.signin);
@@ -11,10 +11,19 @@ module.exports = (app) => {
     failureRedirect: '/signup',
   }));
 
+  app.get('/dashboard', isLoggedIn, authController.dashboard);
+
   app.get('/logout', authController.logout);
 
+  app.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/signin'
+  }));
+
   function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
+    if (req.isAuthenticated())
+      return next();
+
     res.redirect('/signin');
   }
 };
