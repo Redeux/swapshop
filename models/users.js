ba21471c9d8c0c8a848218a7c34fe4b5bc79a036
Phil Sautter <psautter@gmail.com>
2017-06-14 21:28:15 -0400

    working signup and sign-in

--- models/users.js
+++ models/users.js
@@ -1,10 +1,10 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define('User', {
-    // username: {
-    //   type: DataTypes.STRING(15),
-    //   allowNull: false,
-    //   unique: true,
-    // },
+    username: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+      // unique: true,
+    },
     password: {
       type: DataTypes.STRING(64),
       allowNull: false,
@@ -15,41 +15,32 @@ module.exports = function (sequelize, DataTypes) {
       isEmail: true,
       unique: true,
     },
-    // firstName: {
-    //   type: DataTypes.STRING(25),
-    //   allowNull: true,
-    //   is: ['^[a-z]+$', 'i'],
-    // },
-    // lastName: {
-    //   type: DataTypes.STRING(25),
-    //   allowNull: true,
-    //   is: ['^[a-z]+$', 'i'],
-    // },
-    // streetAddress: {
-    //   type: DataTypes.STRING(60),
-    //   allowNull: true,
-    // },
-    // city: {
-    //   type: DataTypes.STRING(30),
-    //   allowNull: true,
-    // },
-    // state: {
-    //   type: DataTypes.STRING(20),
-    //   allowNull: true,
-    // },
-    // zipcode: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: true,
-    // },
-    // createdAt: {
-    //   type: DataTypes.DATE,
-    //   field: 'beginTime',
-    //   defaultValue: sequelize.literal('NOW()'),
-    // },
-    // updatedAt: {
-    //   type: DataTypes.DATE,
-    //   defaultValue: sequelize.literal('NOW()'),
-    // },
+    firstName: {
+      type: DataTypes.STRING(25),
+      allowNull: true,
+      is: ['^[a-z]+$', 'i'],
+    },
+    lastName: {
+      type: DataTypes.STRING(25),
+      allowNull: true,
+      is: ['^[a-z]+$', 'i'],
+    },
+    streetAddress: {
+      type: DataTypes.STRING(60),
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING(30),
+      allowNull: true,
+    },
+    state: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
+    zipcode: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
   }, {
     timestamps: true,
   }, {
