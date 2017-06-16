fa69d2bb1eccf3d0c33104020a3c7c87c347fc4a
E. Seton Raynor <esetonraynor@gmail.com>
2017-06-16 15:45:12 -0400

    front end in progress

--- lib/userSeeds.sql
+++
@@ -1,7 +0,0 @@
-USE swapshop_db;
-
-INSERT INTO users (username, password, emailAddress, firstName, lastName, streetAddress, city, state, zipcode) VALUES ("Gillian_Welch", "Hardtimes1!", "gillian@gmail.com", "Gillian", "Welch", "205 Willow Way", "Los Angeles", "California", "90210");
-
-INSERT INTO users (username, password, emailAddress, firstName, lastName, streetAddress, city, state, zipcode) VALUES ("robsthebest", "abcefghi1", "robyoung@hotmail.com", "Rob", "Young", "9A Canterbury Lane", "Balitmore", "Maryland", "30028");
-
-INSERT INTO users (username, password, emailAddress, firstName, lastName, streetAddress, city, state, zipcode) VALUES ("amphigorey", "wugglyUmp", "edwardgorey@msn.com", "Edward", "Gorey", "21 Koelnstrasse", "Koeln", "Germany", "43902");
